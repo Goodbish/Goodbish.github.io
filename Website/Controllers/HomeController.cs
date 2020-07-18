ea1c61bcdd2b1f95d3816e35ce8c3c0a3b8a1083
даниил банников <goodbish@mail.ru>
2020-07-18 23:26:15 +0500

    hi

---
+++ Website/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Website.Models;
+
+namespace Website.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult redirect()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+    public class portfolioController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public portfolioController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+    public class priceController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public priceController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+    public class contactController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public contactController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Website/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿
+// Write your JavaScript code.
+
+//function redirect() {
+//    let result = confirm('Перейти на страницу Instagram?')
+
+//    if (result === true) {
+//        window.location.href = "https://www.instagram.com/evgenya3_photo/";
+//    }
+//}
